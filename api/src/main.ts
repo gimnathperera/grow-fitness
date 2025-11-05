e1345464d020dfe447fd99f1d145f66a5c3ea4bb
gimnathperera <gimnathperera@gmail.com>
2025-11-05 09:05:58 +0530

    refactor: simplify CORS configuration in main.ts

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,14 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Enable CORS
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+    origin: true,
     credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
-
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
